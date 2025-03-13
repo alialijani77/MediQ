ead05d0cafb16dc3069222b592c29bd2eba71c32
ali <alialijani1377@gmail.com>
2025-03-13 13:02:33 +0330

    Add DependencyContainer

---
+++ MediQ.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MediQ.Infra.Ioc
+{
+	public static class DependencyContainer
+	{
+		public static void RegisterDependencies(IServiceCollection services)
+		{
+		}
+	}
+}
