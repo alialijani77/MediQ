e55c80d13dea56ba226864417c0769f5931ac52a
ali <alialijani1377@gmail.com>
2025-03-14 19:41:59 +0330

    Add RegisterDto

---
+++ MediQ.Core/DTOs/Account/User/RegisterDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediQ.Core.DTOs.Account.User
+{
+	public class RegisterDto
+	{
+		[Display(Name = "ایمیل")]
+		[MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+		[EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد .")]
+		[Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+		public string Email { get; set; }
+
+		[Display(Name = "کلمه عبور")]
+		[Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+		[MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+		public string Password { get; set; }
+
+		[Display(Name = "تکرار کلمه عبور")]
+		[Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+		[MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+		[Compare("Password", ErrorMessage = "کلمه های عبور مغایرت دارند .")]
+		public string RePassword { get; set; }
+	}
+
+	public enum RegisterResult
+	{
+		Success,
+		EmailExists
+	}
+}
