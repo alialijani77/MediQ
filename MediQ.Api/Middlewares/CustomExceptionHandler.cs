96953d052bc69d09880c1778f283846b23b8e245
ali <alialijani1377@gmail.com>
2025-03-23 22:58:42 +0330

    Add CustomExceptionHandler

---
+++ MediQ.Api/Middlewares/CustomExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MediQ.Api.Middlewares
+{
+	// You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+	public class CustomExceptionHandler
+	{
+		private readonly RequestDelegate _next;
+
+		public CustomExceptionHandler(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public Task Invoke(HttpContext httpContext)
+		{
+
+			return _next(httpContext);
+		}
+	}
+
+	// Extension method used to add the middleware to the HTTP request pipeline.
+	public static class CustomExceptionHandlerExtensions
+	{
+		public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+		{
+			return builder.UseMiddleware<CustomExceptionHandler>();
+		}
+	}
+}
