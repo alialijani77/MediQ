d236dfe4e74d9545cdf98f558dce124d7384f140
ali <alialijani1377@gmail.com>
2025-03-29 23:11:11 +0330

    Add UserListDto

---
+++ MediQ.Core/DTOs/Admin/Users/UserListDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MediQ.Core.DTOs.Admin.Users
+{
+	public class UserListDto
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string UserName { get; set; }
+		public string PhoneNumber { get; set; }
+		public string EmailConfirmed { get; set; }
+		public string AccessFailedCount { get; set; }
+	}
+}
