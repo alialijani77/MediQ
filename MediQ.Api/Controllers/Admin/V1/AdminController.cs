f2b86c5db80ec283fade6d7e1f1450f1956d509a
ali <alialijani1377@gmail.com>
2025-04-04 16:23:16 +0330

    Add UpdateUserDto

--- MediQ.Api/Controllers/Admin/V1/AdminController.cs
+++ MediQ.Api/Controllers/Admin/V1/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Asp.Versioning;
 using MediQ.Core.DTOs.Account.User;
+using MediQ.Core.DTOs.Admin.Users;
 using MediQ.Core.DTOs.ApiResult;
 using MediQ.CoreBusiness.Services.Implementions;
 using MediQ.CoreBusiness.Services.Interfaces;
@@ -59,6 +60,23 @@ namespace MediQ.Api.Controllers.Admin.V1
 
 			}
 			var result = await _userService.Register(register);
+
+			if (result)
+			{
+				return new JsonResult(ApiResultDto<bool>.CreateSuccess(true, true, "ایمیل فعالسازی به کاربر افزوده شده ارسال شد."));
+			}
+			throw new Exception(StatusCodes.Status404NotFound.ToString());
+		}
+
+		[HttpPost("UpdateUser")]
+		public async Task<IActionResult> UpdateUser(UpdateUserDto updateuser)
+		{
+			if (!ModelState.IsValid)
+			{
+				throw new Exception(StatusCodes.Status403Forbidden.ToString());
+
+			}
+			var result = await _userService.Register(updateuser);
 
 			if (result)
 			{
---
+++ MediQ.Core/DTOs/Admin/Users/UpdateUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MediQ.Core.DTOs.Admin.Users
+{
+	public class UpdateUserDto
+	{
+		public string Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string PhoneNumber { get; set; }
+	}
+}
