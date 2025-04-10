bcaa64dbc09b963313739b2e552c01a45c558e47
ali <alialijani1377@gmail.com>
2025-04-10 09:51:02 +0330

    getallroles is added

--- MediQ.Domain/Interfaces/IRoleRepository.cs
+++ MediQ.Domain/Interfaces/IRoleRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace MediQ.Domain.Interfaces
+﻿using MediQ.Domain.Entities.UserManagement;
+
+namespace MediQ.Domain.Interfaces
 {
 	public interface IRoleRepository
 	{
+		Task<IList<Role>> GetAllRoles();
+
 	}
 }
--- MediQ.Infra.Data/Repositories/RoleRepository.cs
+++ MediQ.Infra.Data/Repositories/RoleRepository.cs
@@ -1,8 +1,26 @@
-﻿using MediQ.Domain.Interfaces;
+﻿using MediQ.Domain.Entities.UserManagement;
+using MediQ.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
 
 namespace MediQ.Infra.Data.Repositories
 {
 	public class RoleRepository : IRoleRepository
 	{
+		#region ctor
+		private readonly RoleManager<Role> _roleManager;
+
+		public RoleRepository(RoleManager<Role> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+		#endregion
+		#region Roles
+		public async Task<IList<Role>> GetAllRoles()
+		{
+			var result = _roleManager.Roles.ToList();
+			return result;
+		}
+		#endregion
+
 	}
 }
