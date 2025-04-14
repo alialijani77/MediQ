2b613a0210dae3942a5beedc336cb82944dabdca
ali <alialijani1377@gmail.com>
2025-04-14 21:00:37 +0330

    UpdateRoleDto is added

---
+++ MediQ.Core/DTOs/Account/Role/UpdateRoleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MediQ.Core.DTOs.Account.Role
+{
+	public class UpdateRoleDto
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
