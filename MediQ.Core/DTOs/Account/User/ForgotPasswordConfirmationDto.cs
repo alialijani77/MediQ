27e7abdc8e5bfc8f96ab1d32704d561208175528
ali <alialijani1377@gmail.com>
2025-04-19 00:06:14 +0330

    ForgotPasswordConfirmationDto is added

---
+++ MediQ.Core/DTOs/Account/User/ForgotPasswordConfirmationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediQ.Core.DTOs.Account.User
+{
+	public class ForgotPasswordConfirmationDto
+	{
+		[Display(Name = "ایمیل")]
+		[MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+		[EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد .")]
+		[Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+		public string Email { get; set; }
+	}
+}
