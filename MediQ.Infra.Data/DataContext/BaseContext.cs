36a1a141adb2f69369a724ac02f76bccc4f3be83
Amin Saffarnejad <vira1368@gmail.com>
2025-04-27 16:47:57 +0330

    Refactor BaseContext for int primary key and configurations
    Updated BaseContext to use int as the primary key type for IdentityDbContext. Added configuration classes for EmailSetting, Role, and User in the OnModelCreating method, replacing inline data seeding. Updated using directives to include new configuration namespaces.

--- MediQ.Infra.Data/DataContext/BaseContext.cs
+++ MediQ.Infra.Data/DataContext/BaseContext.cs
@@ -1,48 +1,36 @@
 ﻿using MediQ.Domain.Entities.Common;
 using MediQ.Domain.Entities.UserManagement;
+using MediQ.Infra.Data.Configurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace MediQ.Infra.Data.DataContext
 {
-	public abstract class BaseContext : IdentityDbContext<User, Role, string>
-	{
-		protected BaseContext() { }
+    public abstract class BaseContext : IdentityDbContext<User, Role, int>
+    {
+        protected BaseContext() { }
 
-		public static BaseContext CreateInstance(string connectionString, ILoggerFactory loggerFactory)
-		{
-			return new SqlServerContext(connectionString, loggerFactory);
-		}
+        public static BaseContext CreateInstance(string connectionString, ILoggerFactory loggerFactory)
+        {
+            return new SqlServerContext(connectionString, loggerFactory);
+        }
 
-		#region DbSet
-		public DbSet<EmailSetting> EmailSettings { get; set; }
+        #region DbSet
+        public DbSet<EmailSetting> EmailSettings { get; set; }
 
-		#endregion
+        #endregion
 
-		protected override void OnModelCreating(ModelBuilder builder)
-		{
-			base.OnModelCreating(builder);
-			builder.HasDefaultSchema("MediQ");
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("MediQ");
 
-			#region SeadData
-			var date = DateTime.MinValue;
-
-			builder.Entity<EmailSetting>().HasData(new EmailSetting()
-			{
-				CreateDate = date,
-				DisplayName = "MediQ",
-				EnableSSL = true,
-				From = "mediqsystem@gmail.com",
-				Id = 1,
-				IsDefault = true,
-				IsDelete = false,
-				Password = "tkisthnoqbtvsmlq",
-				Port = 587,
-				SMTP = "smtp.gmail.com"
-
-			});
-			#endregion
-		}
-	}
+            #region Configurations
+            new EmailSettingConfiguration().Configure(builder.Entity<EmailSetting>());
+            new RoleConfiguration().Configure(builder.Entity<Role>());
+            new UserConfiguration().Configure(builder.Entity<User>());
+            #endregion
+        }
+    }
 }
