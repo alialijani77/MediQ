805ba20326c8d648c57e14a2d498659655a338d5
Amin Saffarnejad <vira1368@gmail.com>
2025-04-27 16:47:03 +0330

    Add RoleConfiguration for Entity Framework setup.

---
+++ MediQ.Infra.Data/Configurations/RoleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using MediQ.Domain.Entities.UserManagement;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MediQ.Infra.Data.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            List<Role> roles =
+            [
+                new() { Id = 1, Name = "Admin" },
+                new() { Id = 2, Name = "User" }
+            ];
+
+            builder
+                .ToTable("Roles")
+                .HasData(roles);
+        }
+    }
+}
