cdc5437e504bef14023fa0736b70eb86e1b6470f
Amin Saffarnejad <vira1368@gmail.com>
2025-04-27 16:47:35 +0330

    Add EmailSettingConfiguration for EF Core setup. Seeding it with a default EmailSetting instance with predefined values.

---
+++ MediQ.Infra.Data/Configurations/EmailSettingConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using MediQ.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MediQ.Infra.Data.Configurations
+{
+    public class EmailSettingConfiguration : IEntityTypeConfiguration<EmailSetting>
+    {
+        public void Configure(EntityTypeBuilder<EmailSetting> builder)
+        {
+            EmailSetting emailSetting = new()
+            {
+                CreateDate = DateTime.Now,
+                DisplayName = "MediQ",
+                EnableSSL = true,
+                From = "mediqsystem@gmail.com",
+                Id = 1,
+                IsDefault = true,
+                IsDelete = false,
+                Password = "tkisthnoqbtvsmlq",
+                Port = 587,
+                SMTP = "smtp.gmail.com"
+            };
+
+            builder
+                .ToTable("EmailSettings")
+                .HasData(emailSetting);
+        }
+    }
+}
