e7f40f011565bb67900c154957bdcbc1bd12a49b
Amin Saffarnejad <vira1368@gmail.com>
2025-04-27 16:46:41 +0330

    Add UserConfiguration for Entity Framework setup. Seeding it with initial data.

---
+++ MediQ.Infra.Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using MediQ.Domain.Entities.UserManagement;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MediQ.Infra.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            var user = new User()
+            {
+                Id = 1,
+                UserName = "TestUser@gmail.com",
+                Email = "TestUser@gmail.com",
+                EmailConfirmed = true,
+            };
+
+            var passwordHasher = new PasswordHasher<User>();
+            passwordHasher.HashPassword(user, "P@ssword 1234");
+
+            builder
+                .ToTable("Users")
+                .HasData(user);
+        }
+    }
+}
