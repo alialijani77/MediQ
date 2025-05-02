c291b1e40e992d37d51c59fb00ff4c612fd86062
ali <alialijani1377@gmail.com>
2025-05-03 00:02:55 +0330

    Visitor is added

---
+++ MediQ.Domain/Entities/Common/Visitor.cs
@@ -0,0 +1,31 @@
+﻿namespace MediQ.Domain.Entities.Common
+{
+	public class Visitor
+	{
+		public string Ip { get; set; }
+		public string CurrentLink { get; set; }
+		public string ReferrerLink { get; set; }
+		public string Method { get; set; }
+		public string Protocol { get; set; }
+		public string PhysicalPath { get; set; }
+		public VisitorVersion Browser { get; set; }
+		public VisitorVersion OperationSystem { get; set; }
+		public Device Device { get; set; }
+
+	}
+
+	public class VisitorVersion
+	{
+		public string Family { get; set; }
+		public string Version { get; set; }
+	}
+
+	public class Device
+	{
+		public string Brand { get; set; }
+		public string Family { get; set; }
+		public string Model { get; set; }
+		public bo IsSpider { get; set; }
+
+	}
+}
